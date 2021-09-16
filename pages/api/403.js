ffcaebb5c3a0664de5ce0071fc13e18d8c36a294
Luca Gandini <lgandini@funambol.com>
2021-09-16 11:40:21 +0200

    Cors for api route

--- pages/api/403.js
+++ pages/api/403.js
@@ -1,3 +1,24 @@
-export default function handler(req, res) {
+import Cors from 'cors';
+
+const cors = Cors({
+  credentials: true,
+  methods: ['GET', 'HEAD', 'OPTIONS', 'POST']
+});
+
+const middleware = (req, res, fn) => {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+
+      return resolve(result)
+    })
+  })
+};
+
+export default async function handler(req, res) {
+  await middleware(req, res, cors);
+
   res.status(403).send("Forbidden");
 }
